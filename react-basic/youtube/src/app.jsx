320d37b8606cc98a0967d1a6ac6d8384c780fc03
seambark <run128@naver.com>
2021-01-14 21:34:23 +0900

    API이용하여 리스트 만들기

--- react-basic/youtube/src/app.jsx
+++ react-basic/youtube/src/app.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
-function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+function App(){
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDsDCGUfD7P5_-qCwcYyABwvSjNLJKF22w", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  },[]);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
---
+++ react-basic/youtube/src/components/video_item/video_item.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const VideoItem = props => <li><strong>{props.video.snippet.title}</strong></li>
+
+export default VideoItem;
