80f01ee6d1a5d764d8404aae00af687b4362f049
seambark <run128@naver.com>
2021-01-16 22:58:07 +0900

    아이콘 변경

--- react-basic/youtube/src/components/header.jsx
+++ react-basic/youtube/src/components/header.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { memo, useRef } from 'react';
 import styles from './header.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 
-const Header = props => (
-<header>
-    <h1 className={styles.logo}><i className="fab fa-youtube"></i>Youtube</h1>
-    <div className={styles.search_box}>
-        <input type="search"/>
-        <button><i className="fas fa-search"></i>검색</button>
-    </div>
-</header>
-);
+const Header = memo(props => {
+    const formRef = useRef();
+    const searchValue = useRef();
+
+    const onSubmit = event => {
+        event.preventDefault();
+        const search = searchValue.current.value;
+        console.log(search);
+    }
+
+    return (
+        <header>
+            <h1 className={styles.logo}><FontAwesomeIcon icon={faYoutube}/>Youtube</h1>
+            <form className={styles.search_box} ref={formRef} onSubmit={onSubmit}>
+                <input type="search" ref={searchValue}/>
+                <button><FontAwesomeIcon icon={faSearch} />검색</button>
+            </form>
+        </header>
+    );
+});
 
 export default Header;
