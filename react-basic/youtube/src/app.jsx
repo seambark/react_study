6b247f53beb1d4b6c5434beb731e4f25cc4d8e1d
seambark <run128@naver.com>
2021-01-18 05:06:56 +0900

    검색기능 리팩토링

--- react-basic/youtube/src/app.jsx
+++ react-basic/youtube/src/app.jsx
@@ -3,33 +3,18 @@ import './app.css';
 import VideoList from './components/video_list/video_list';
 import Header from './components/header';
 
-function App(){
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDsDCGUfD7P5_-qCwcYyABwvSjNLJKF22w`, requestOptions)
-      .then(response => response.json())
-      .then(result => 
-        result.items.map(item => ({...item, id: item.id.videoId}))
-      )
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDsDCGUfD7P5_-qCwcYyABwvSjNLJKF22w", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));
   },[]);
   return (
     <>
