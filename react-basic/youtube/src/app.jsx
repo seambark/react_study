53fb5268cfe1be63cdd73b2f9c666a9a418249f2
seambark <run128@naver.com>
2021-01-19 23:57:15 +0900

    header 폴더구조 변경

--- react-basic/youtube/src/app.jsx
+++ react-basic/youtube/src/app.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './app.css';
 import VideoList from './components/video_list/video_list';
-import Header from './components/header';
+import Header from './components/header/header';
 
 function App({youtube}) {
   const [videos, setVideos] = useState([]);
