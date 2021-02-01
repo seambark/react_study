3870b7076cb437483c9333a3ed9d0c3e47a9adcc
seambark <run128@naver.com>
2021-02-01 23:03:43 +0900

    로그인 html,css 작업

--- react-basic/card-maker/src/app.jsx
+++ react-basic/card-maker/src/app.jsx
@@ -1,8 +1,9 @@
 import './app.css';
+import Login from './components/login';
 
 function App() {
   return (
-    <h1>Hello :)</h1>
+    <Login />
   );
 }
 
---
+++ react-basic/card-maker/src/components/login.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './login.module.css'
+
+const Login = props => (
+    <div className={styles.loginArea}>
+        <header>
+            <h2>Business Card Maker</h2>
+        </header>
+        <div className={styles.content}>
+            <h3>Login</h3>
+            <ul>
+                <li><button type="button">Google</button></li>
+                <li><button type="button">Github</button></li>
+            </ul>
+        </div>
+        <footer>
+            <p>Code your dream</p>
+        </footer>
+    </div>
+);
+
+export default Login;
