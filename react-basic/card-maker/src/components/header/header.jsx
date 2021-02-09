7fbae5e01adbd4af02d35983598e34b783802233
seambark <run128@naver.com>
2021-02-09 23:34:17 +0900

    logout 버튼추가

--- react-basic/card-maker/src/components/header/header.jsx
+++ react-basic/card-maker/src/components/header/header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import styles from './header.module.css';
 
-const Header = props => (
+const Header = ({onLogout}) => (
     <header>
+        { onLogout && <button className={styles.btnLogout} onClick={onLogout}>Logout</button> }
         <img src="/images/logo.png" alt=""/>
         <h2>Business Card Maker</h2>
     </header>
